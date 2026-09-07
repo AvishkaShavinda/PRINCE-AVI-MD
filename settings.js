dc290bf06ee016c01a179056928728b87e4cd671
AVISHKA SHAVINDA <134716496+AvishkaShavinda@users.noreply.github.com>
2026-09-07 20:56:40 +0530

    © 𝙰𝚕𝚙𝚑𝚊 𝚅𝚒𝚜𝚒𝚘𝚗 𝙸𝚗𝚏𝚒𝚗𝚒𝚝𝚢

--- settings.js
+++ settings.js
@@ -25,58 +25,7 @@ global.auto_react = true // හැම මැසේජ් එකකටම රි�
 global.react_emojis = ['❤️', '🩵', '🦄', '👻', '🧚', '💐', '🪄', '⭐', '🅰️', '🇱🇰'] // රියැක්ට් වෙන්න ඕන ඉමෝජි ටික මෙතනට දාන්න
 
 
-global.fake = {
-	anonim: 'https://files.catbox.moe/hhvdoi.jpg',
-	thumbnailUrl: 'https://files.catbox.moe/hhvdoi.jpg',
-	thumbnail: fs.readFileSync('./src/media/Avi.jpg'),
-	docs: fs.readFileSync('./src/media/fake.pdf'),
-	listfakedocs: ['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet','application/vnd.openxmlformats-officedocument.presentationml.presentation','application/vnd.openxmlformats-officedocument.wordprocessingml.document','application/pdf'],
-}
 
-global.my = {
-	yt: 'https://youtube.com/Avi_Nethunter',
-	gh: 'https://github.com/avishka33',
-	gc: 'https://whatsapp.com/channel/0029VaGisHDFHWq5jpuEmC2t',
-	ch: '120363358746473104@newsletter',
-}
-
-global.limit = {
-	free: 20,
-	premium: 999,
-	vip: 9999
-}
-
-global.money = {
-	free: 10000,
-	premium: 1000000,
-	vip: 10000000
-}
-
-global.mess = {
-	key: 'Apikey mu telah habis silahkan kunjungi\nhttps://my.hitori.pw',
-	owner: 'This feature can be used by owner only!',
-	admin: 'This feature can be used by admin only!',
-	botAdmin: 'This feature can only be used when the bot is a group admin!',
-	group: 'This feature is only for groups',
-	private: 'This feature is only for private chats',
-	limit: 'Limit!',
-	prem: 'This feature can be used by premium user only',
-	wait: 'In process...',
-	error: 'Error!',
-	done: 'Done'
-}
-
-global.APIs = {
-	hitori: 'https://api.hitori.pw',
-}
-global.APIKeys = {
-	'https://api.hitori.pw': 'htrkey-77eb83c0eeb39d40',
-	geminiApikey: ['AIzaSyD0lkGz6ZhKi_MHSSmJcCX3wXoDZhELPaQ','AIzaSyDnBPd_EhBfr73NssnThVQZYiKZVhGZewU','AIzaSyA94OZD-0V4quRbzPb2j75AuzSblPHE75M','AIzaSyB5aTYbUg2VQ0oXr5hdJPN8AyLJcmM84-A','AIzaSyB1xYZ2YImnBdi2Bh-If_8lj6rvSkabqlA']
-}
-
-
-global.badWords = ['fuck you']
-global.chatLength = 1000
 
 
 
